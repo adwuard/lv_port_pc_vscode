c2236ed22e69aca5f6da2f7034b2501a1cfbcf28
Hsuan Han Lai <huate.lai@tuya.com>
2025-08-06 20:20:27 +0800

    refactor

--- main/src/main.c
+++ main/src/main.c
@@ -15,7 +15,7 @@
 #include "lvgl/lvgl.h"
 #include "lvgl/examples/lv_examples.h"
 #include "lvgl/demos/lv_demos.h"
-#include "../ai-pocket-pet/lv_demo_ai_pocket_pet.h"
+#include "../ai-pocket-pet/ai_pocket_pet_app.h"
 #include "glob.h"
 
 /*********************
